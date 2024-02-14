ca07ff543e7479e382a972772f6cb2f32f4460a6
anil85290 <145982630+anil85290@users.noreply.github.com>
2024-02-14 14:23:18 +0530

    middleware

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
-const http = require('http');
-const routes = require('./routes')
-const server = http.createServer(routes);
+// const http = require('http');
+// const routes = require('./routes')
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+    console.log('in the middleware')
+    next();
+});
+app.use((req, res, next) => {
+    console.log('2nd the middleware')
+    res.send('<h1> hello from express </h1>')
+});
+const server = http.createServer(app);
 server.listen(3000);
