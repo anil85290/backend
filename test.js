6e2ca10540c674ae82f2030f9316271cdcbcbe3b
anil85290 <145982630+anil85290@users.noreply.github.com>
2024-02-16 15:00:57 +0530

    routes

--- test.js
+++ test.js
@@ -1,24 +1,19 @@
 // const http = require('http');
 // const routes = require('./routes')
 const express = require('express');
-const app = express();
 const bodyparser = require('body-parser')
+const app = express();
+const adminroutes = require('./routes/admin')
+const shoproutes = require('./routes/shop');
+
+
 
 app.use(bodyparser.urlencoded({extended: false}));
+app.use('/admin', adminroutes);
+app.use(shoproutes);
 
-
-app.use( '/add-product',(req, res, next) => {
-    
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="price">  <button type="submit">Add Product </button> </form>');
+app.use( (req,res,next) => {
+    res.status(404).send('<h1>page not found</h1>');
 });
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/', (req, res, next) => {
-    console.log('in the middleware');
-});
 app.listen(3000);
-// const server = http.createServer(app);
-// server.listen(3000);
