9d0524b6f38d17dfc784c5df1ab76fcd94c05402
anil85290 <145982630+anil85290@users.noreply.github.com>
2024-02-16 13:04:37 +0530

    product price

--- test.js
+++ test.js
@@ -2,13 +2,23 @@
 // const routes = require('./routes')
 const express = require('express');
 const app = express();
-app.use((req, res, next) => {
-    console.log('in the middleware')
-    next();
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.urlencoded({extended: false}));
+
+
+app.use( '/add-product',(req, res, next) => {
+    
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="price">  <button type="submit">Add Product </button> </form>');
 });
-app.use((req, res, next) => {
-    console.log('2nd the middleware')
-    res.send('<h1> hello from express </h1>')
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
-const server = http.createServer(app);
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log('in the middleware');
+});
+app.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000);
